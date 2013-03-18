bd7f6d08c6994212dc6923b23589825def580002
Ryan Van Etten <ryanve@gmail.com>
2013-03-18 19:45:27 -0400

    version 0.x

---
+++ path.php
@@ -0,0 +1,288 @@
+<?php 
+/**
+ * @package   airve/path
+ * @link      https://github.com/airve/path
+ * @version   0.x
+ * @license   MIT
+ * @copyright 2013 Ryan Van Etten
+ */
+
+namespace airve;
+
+abstract class Path {
+    
+    protected static $mixins = array();
+
+    public static function mixin($name, $fn = null) {
+        if (\is_scalar($name))
+            $fn and static::$mixins[$name] = $fn;
+        else foreach ($name as $k => $v)
+            static::mixin($k, $v);
+    }
+    
+    # php.net/manual/en/language.oop5.overloading.php#object.callstatic
+    public static function __callStatic($name, $params) {
+        if (isset(static::$mixins[$name]))
+            return \call_user_func_array(static::$mixins[$name], $params);
+        \trigger_error(__CLASS__ . "::$name is not callable.");
+    }
+    
+    /**
+     * @param   string  $name
+     * @return  string
+     */
+    public static function method($name) {
+        return __CLASS__ . "::$name";
+    }
+    
+    /**
+     * @param   mixed  $fn
+     * @param   mixed  $value
+     * @return  mixed
+     */
+    protected static function pass($fn, $value = null) {
+        return null === $fn ? $value : \call_user_func_array(
+            \array_shift($params = func_get_args()), $params
+        );
+    }
+    
+   /**
+    * @param   string  $str 
+    * @return  string
+    */
+    public static function lslash($str) {
+        return '/' . \ltrim($str, '/\\');
+    }
+   
+   /**
+    * @param   string  $str 
+    * @return  string
+    */   
+    public static function rslash($str) {
+        return \rtrim($str, '/\\') . '/';
+    }
+    
+    /**
+     * Join paths or URI parts using a fwd slash as the glue.
+     * @return  string
+     */
+    public static function join() {
+        $result = '';
+        foreach (\func_get_args() as $n)
+            $result = $result ? \rtrim($result, '/\\') . '/' . \ltrim($n, '/\\') : $n;
+        return $result;
+    }
+    
+    /**
+     * @return  string
+     */   
+    public static function root($pathRelative) {
+        return $_SERVER['DOCUMENT_ROOT'] . static::lslash($pathRelative);
+    }
+    
+    /**
+     * @return  string
+     */
+    public static function dir($pathRelative) {
+        return __DIR__ . static::lslash($pathRelative);
+    }
+
+    /**
+     * @param   string   $path 
+     * @param   string   $scheme 
+     * @return  string
+     */
+    public static function toUri($path, $scheme = 'http') {
+        $uri = ($scheme ? $scheme . '://' : '//') . $_SERVER['SERVER_NAME'];
+        return static::join($uri, \str_replace($_SERVER['DOCUMENT_ROOT'], '', $path));
+    }
+    
+    /**
+     * Get the modified time of a file or a directory. For directories,
+     * it gets the modified time of the most recently modified file.
+     * @param   string       $path     Full path to directory or file.
+     * @param   string       $format   Date string for use with date()
+     * @return  number|string|null
+     */
+    public static function mtime($path, $format = null) {
+        $time = \array_map('filemtime', static::listPaths($path));
+        $time = $time ? \max($time) : null;
+        return $format && $time ? \date($format, $time) : $time;
+    }
+    
+    /**
+     * @return  bool
+     */
+    public static function isPath($item) {
+        return \ctype_print($item) && \is_readable($item);
+    }
+    
+    /**
+     * @return  bool
+     */
+    public static function isDir($item) {
+        return static::isPath($item) && \is_dir($item);
+    }
+    
+    /**
+     * @return  bool
+     */
+    public static function isFile($item) {
+        return static::isPath($item) && ! \is_dir($item);
+    }
+
+    /**
+     * Test if item consists entirely of dots 
+     * @return  bool
+     */
+    public static function isDot($item) {
+        return \ctype_punct($item) && !\ltrim($item, '.');
+    }
+    
+    /**
+     * @return  bool
+     */
+    public static function isAbs($item) {
+        return \ctype_print($item) && \realpath($item) === $item;
+    }
+    
+    /**
+     * @return  string|bool
+     */
+    public static function toAbs($item) {
+        return \ctype_print($item) ? \realpath($item) : false;
+    }
+
+    /**
+     * @return array
+     */
+    public static function listPaths($path) {
+        $list = array();
+        foreach (\scandir($path) as $n)
+            static::isDot($n) or $list[] = $n;
+        return $list;
+    }
+    
+    /**
+     * @return array
+     */
+    public static function listDirs($path) {
+        return \array_filter(static::listPaths($path), 'is_dir');
+    }
+
+    /**
+     * @return array
+     */
+    public static function listFiles($path) {
+        $list = array();
+        $base = static::rslash($path);
+        foreach (static::listPaths($path) as $n) {
+            if (\is_dir($base . $n)) {
+                foreach (static::listFiles($base . $n) as $file)
+                    $list[] = static::join($n, $file);
+            } else { $list[] = $n; }
+        }
+        return $list ? static::sort($list) : $list;
+    }
+    
+    /**
+     * @return array
+     */
+    public static function group(array $list) {
+        $levels = array(); #map
+        foreach ($list as $i => $n)
+            $levels[$i] = \substr_count($n, '/');
+        # Ensure result is ordered and non-sparse.
+        $result = \array_pad(array(), \max($levels), array());
+        foreach ($list as $i => $n)
+            $result[$levels[$i]][] = $n;
+        return $result;
+    }
+    
+    /**
+     * @return array
+     */
+    public static function sort(array $list) {
+        return \call_user_func_array('array_merge', \array_reverse(static::group($list)));
+    }
+    
+    /**
+     * @return  mixed
+     */
+    public function find($list, callable $test) {
+        foreach($list as $k => $v)
+            if (\call_user_func($test, $v, $k, $list))
+                return $v;
+    }
+    
+    /** 
+     * @return string
+     */
+    public static function findPath($path, callable $test) {
+        return static::find(static::listPaths($path), $test);
+    }
+    
+    /** 
+     * @return string
+     */    
+    public static function findFile($path, callable $test) {
+        return static::find(static::listFiles($path), $test);
+    }
+    
+    /** 
+     * @return string
+     */
+    public static function findDir($path, callable $test) {
+        return static::find(static::listDirs($path), $test);
+    }
+
+    
+    /** 
+     * @return mixed
+     */
+    public static function getFile($path, callable $fn = null) {
+        return static::pass($fn, static::isFile($path) ? \file_get_contents($path) : false);
+    }
+    
+    /** 
+     * @return mixed
+     */
+    public static function putFile($path, $data) {
+        return \ctype_print($path) ? \file_put_contents($path, 
+            $data instanceof \Closure ? $data(static::getFile($path)) : $data
+        ) : false;
+    }
+    
+    /** 
+     * @return object|null
+     */
+    public static function getJson($path, callable $fn = null) {
+        return static::pass($fn, \is_scalar($path) ? \json_decode(
+            \file_get_contents($path)
+        ) : (null === $path ? null : (object) $path));
+    }
+    
+    /** 
+     * @return mixed
+     */
+    public static function putJson($path, $data) {
+        if ( ! \ctype_print($path))
+            return false;
+        $data instanceof \Closure and $data = $data(static::getJson($path));
+        return \file_put_contents($path, \is_string($data) ? $data : \json_encode($data));
+    }
+    
+    /** 
+     * @return mixed
+     */
+    public static function loadFile($path, callable $fn = null) {
+        if (static::isFile($path)) {
+            \ob_start(); 
+            include $path;
+            $path = \ob_get_contents();
+            \ob_end_clean();
+        } else { $path = false; }
+        return static::pass($fn, $path);
+    }
+    
+}#class
